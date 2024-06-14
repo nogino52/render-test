356a734ecc3c0a1c361eabc47176d902a0697f02
ゲムシャリコ <gameshalico@outlook.jp>
2024-06-14 10:51:17 +0900

    feat: add get omikuji api

--- main.py
+++ main.py
@@ -12,3 +12,20 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/omikuji")
+def omikuji():
+    omikuji_list = [
+        "大吉",
+        "中吉",
+        "小吉",
+        "吉",
+        "半吉",
+        "末吉",
+        "末小吉",
+        "凶",
+        "小凶",
+        "大凶"
+    ]
+
+    return omikuji_list[random.randrange(10)]
